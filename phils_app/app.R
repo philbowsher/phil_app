18ac959983fad908a914e4819832b24e7006a402
Phil Bowsher <phil@rstudio.com>
2021-06-21 20:48:08 +0000

    new title

--- phils_app/app.R
+++ phils_app/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Old Faithful Geyser Data phils app"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
